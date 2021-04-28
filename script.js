6acd36622fa1e7bf158b4a443b2ee154d97e60af
Neely <neely@Neelys-MBP.HPH.Local>
2021-04-28 08:49:52 -0500

    add phrase spotlight

--- script.js
+++ script.js
@@ -15,27 +15,37 @@ let maxWrong = 6;
 let mistakes = 0;
 let guessed = [];
 //function to include is display guessed letters on the page.
-// let wordStatus = null;
+let wordStatus = null;
 //what is wordStatus for?
 
 function randomnPharse() {
 	answer = phrases[Math.floor(Math.random() * phrases.length)];
 }
 function generateButtons() {
-	let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'
+	let buttonsHTML = 'qwertyuiopasdfghjklzxcvbnm'
 		.split('')
 		.map(
 			(letter) =>
 				`
     
-    <button type="button" class="btn btn-danger"
+    <button type="button" class="btn btn-danger m-3"
     id='` + letter + `' onClick='handleGuess('` + letter +`')'>` + letter + `</button>`
 		)
-		.join('\n');
+		.join('');
 	document.getElementById('keyboard').innerHTML = buttonsHTML;
 }
 //map creates a new array populated with the results of calling a provided function on every element in the calling array
+function guessedWord(){
+	wordStatus = answer
+		.split('')
+		.map((letter) => (guessed.indexOf(letter) >= 0 ? letter : ' _ '))
+		.join('');
+	//The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+	document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
 document.getElementById('max-wrong').innerHTML = maxWrong;
 
 randomnPharse();
 generateButtons();
+guessedWord();

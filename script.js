2a09365f97d920ec32ae87a100d23662922227f7
Neely <neely@Neelys-MBP.HPH.Local>
2021-04-28 14:09:37 -0500

    create modal with instructions

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-let colors = ['pink', 'green', 'blue', 'brown'];
-let Hints = ['rabbit','the color of moss', 'the sky','wood']
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+	modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
+
+let colors = ['pink', 'green', 'blue', 'brown', 'cerulean'];
+let Hints = [
+	'was once assigned to the male gender',
+	'the color of moss',
+	'the sky',
+	'wood',
+	'miranda Priestly had a monologue about this color ',
+];
 let answer = '';
 let maxWrong = 4;
 let mistake = 0;
@@ -43,7 +75,7 @@ function handleGuess(chosenLetter) {
 		mistake++;
 		updateMistakes();
 		checkIfGameLost();
-        updatePicture();
+		updatePicture();
 	}
 }
 function guessedWord() {
@@ -64,7 +96,7 @@ function checkIfGameWon() {
 		document.getElementById('keyboard').innerHTML = 'You won';
 		document.getElementById('title').style.display = 'none';
 		document.getElementById('wordSpotlight').style.display = 'none';
-        document.getElementById('hintSpotlight').style.display = 'none';
+		document.getElementById('hintSpotlight').style.display = 'none';
 	}
 }
 
@@ -72,28 +104,27 @@ function checkIfGameLost() {
 	if (mistake === maxWrong) {
 		document.getElementById('keyboard').innerHTML = 'You Lost';
 		document.getElementById('title').style.display = 'none';
-		document.getElementById('wordSpotlight').innerHTML= 'The answer was: ' + answer;
+		document.getElementById('wordSpotlight').innerHTML =
+			'The answer was: ' + answer;
 	}
 }
-function reset(){
-    mistake = 0;
-    guessed= [];
-    document.getElementById('base').src = '/images/png/0.png'
-    randomPhrase();
-    guessedWord();
-    updateMistakes();
-    generateButtons();
+function reset() {
+	mistake = 0;
+	guessed = [];
+	document.getElementById('base').src = '/images/png/0.png';
+	randomPhrase();
+	guessedWord();
+	updateMistakes();
+	generateButtons();
 }
-function updatePicture(){
-    document.getElementById('base').src = '/images/png/' + mistake+'.png';
+function updatePicture() {
+	document.getElementById('base').src = '/images/png/' + mistake + '.png';
 }
 
-function giveHint(){
-    document.getElementById('hintSpotlight').innerHTML =
-			Hints[colors.indexOf(answer)];
-    //the hint index will equal the value of color array
-
-
+function giveHint() {
+	document.getElementById('hintSpotlight').innerHTML =
+		Hints[colors.indexOf(answer)];
+	//the hint index will equal the value of color array
 }
 document.getElementById('max-wrong').innerHTML = maxWrong;
 
